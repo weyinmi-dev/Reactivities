365ec771bfb0679a1ea2698858787aa67f50d64c
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2025-06-06 21:06:23 +0100

    Feat: endo of section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ActivitiesController : BaseApiController
+{
+    private readonly DataContext _context;
+    public ActivitiesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet] // api/activities
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await _context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")] // api/activities/{id}
+    public async Task<IActionResult> GetActivity(Guid id)
+    {
+        return await _context.Activities.FindAsync(id) is Activity activity
+            ? Ok(activity)
+            : NotFound();
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+    // Seed data can be added here if needed
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
+app.Run();
