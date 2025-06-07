e70351a5a6390cc0045faa78d0a18d9477a3cf7d
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2025-06-07 16:40:37 +0100

    feat: CRUD operations created for Activities endpoint

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers;
 public class ActivitiesController : BaseApiController
 {
     // api/activities
-    [HttpGet] 
+    [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
         return await Mediator.Send(new List.Query());
@@ -20,6 +20,31 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return await Mediator.Send(new Details.Query { Id = id }); 
+        return await Mediator.Send(new Details.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
+    {
+        await Mediator.Send(new Create.Command { Activity = activity });
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, [FromBody] Activity activity)
+    {
+        if (id != activity.Id)
+        {
+            return BadRequest("Activity ID mismatch");
+        }
+        await Mediator.Send(new Edit.Command { Activity = activity });
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActivity(Guid id)
+    {
+        await Mediator.Send(new Delete.Command { Id = id });
+        return Ok();
     }
 }
