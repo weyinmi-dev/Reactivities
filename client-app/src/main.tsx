0b94bd6b9a4447c248a352a1c2c099b66b52165b
weyinmi-dev <weyinmi.e.madamedon@gmail.com>
2025-06-07 17:55:37 +0100

    Folder-structure changes and Creation of Interface Type

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
-import "./index.css";
-import App from "./App.tsx";
+import "./app/layout/styles.css";
+import App from "./app/layout/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
